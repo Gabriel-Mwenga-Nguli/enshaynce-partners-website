7376063d38148c11606693b4303db3e0e90f1cc1
gabe gfx <gabegfxkenya@gmail.com>
2025-07-04 08:48:39 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@opentelemetry/sdk-node', 'handlebars'],
+  },
   images: {
     remotePatterns: [
       {
